32c1e939e54af65772d3d3e3fe6ff6974c116717
Elybourland <JEBourland@yahoo.com>
2022-11-10 19:50:35 -0800

    passing props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:54PM" 
+        content="Nice blog post!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 8:54PM" 
+        content="I concur!" 
+        avatar={faker.image.avatar()} 
+      />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Yesterday at 2:54AM" 
+        content="Wow! Thank you so much." 
+        avatar={faker.image.avatar()} 
+      />
     </div>
   );
 };
