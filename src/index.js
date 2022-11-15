f1094e00dc370f9930e29d154852e65565240678
Elybourland <JEBourland@yahoo.com>
2022-11-14 20:09:03 -0800

    component reuse

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
+
+      <ApprovalCard>
         <CommentDetail 
           author="Sam" 
           timeAgo="Today at 4:54PM" 
@@ -15,18 +20,22 @@ const App = () => {
           avatar={faker.image.avatar()} 
         />
       </ApprovalCard>
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 8:54PM" 
-        content="I concur!" 
-        avatar={faker.image.avatar()} 
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 2:54AM" 
-        content="Wow! Thank you so much." 
-        avatar={faker.image.avatar()} 
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 8:54PM" 
+          content="I concur!" 
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail 
+          author="Jane" 
+          timeAgo="Yesterday at 2:54AM" 
+          content="Wow! Thank you so much." 
+          avatar={faker.image.avatar()} 
+        />
+      </ApprovalCard>
     </div>
   );
 };
